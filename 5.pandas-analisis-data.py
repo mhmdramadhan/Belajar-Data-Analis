7400e6081572739818225580d3fe06c14582896e
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-06-26 15:03:54 +0700

    Refactor grouping operations in pandas analysis to use agg functions and custom range function

--- 5.pandas-analisis-data.py
+++ 5.pandas-analisis-data.py
@@ -26,6 +26,24 @@ df_group = df.query(query)
 # grupby berdasarkan Type 1
 # df_groupby = df.groupby('Type 1').mean(numeric_only=True)
 # grupby berdasarkan Type 1 dan Type 2
-df_groupby = df.groupby(['Type 1', 'Type 2']).mean(numeric_only=True)
-print(df_groupby)
+# df_groupby = df.groupby(['Type 1', 'Type 2']).mean(numeric_only=True)
+# print(df_groupby)
 # df_groupby.to_excel('sample_data/pokemon_groupby.xlsx')
+
+# group by dengan agg
+df_groupby = df.groupby('Type 1').agg({
+    'Attack': 'max',
+    'Defense': 'min',
+    'Speed': 'mean',
+    'Total': ['max', 'min']
+})
+# print(df_groupby)
+
+# membuat fungsi sendiri
+def range_func(x):
+    return x.max() - x.min()
+
+df_groupby = df.groupby('Type 1').agg({
+    'Sp. Atk': range_func,
+})
+# print(df_groupby)

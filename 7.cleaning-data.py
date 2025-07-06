85d89a51bdf0744195adfa02d309d05649c8ae85
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-07-06 20:55:47 +0700

    Enhance data cleaning script with interpolation and visualization for missing values

--- 7.cleaning-data.py
+++ 7.cleaning-data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import missingno as msno
+import matplotlib.pyplot as plt
 
 # cleaning data
 data = {
@@ -10,7 +12,7 @@ data = {
 
 df = pd.DataFrame(data)
 
-print(df)
+# print(df)
 
 # mengetahui data kosong
 # print(df.isnull())
@@ -27,11 +29,32 @@ print(df)
 
 # mengisi data kosong
 df.fillna(df.mean(), inplace=True)  # mengisi data kosong dengan 0
-print(df)
+# print(df)
 
 # mengisi data kosong dengan nilai sebelumnya dan sesudahnya
-df.fillna(method='ffill', inplace=True)  # mengisi data kosong dengan nilai sebelumnya
-df.fillna(method='bfill', inplace=True)  # mengisi data kosong dengan nilai sesudahnya
-print(df)
+# df.fillna(method='ffill', inplace=True)  # mengisi data kosong dengan nilai sebelumnya
+# df.fillna(method='bfill', inplace=True)  # mengisi data kosong dengan nilai sesudahnya
+# print(df)
 
 
+data2 = {
+    'A' : [1, 2, None, 5],
+    'B' : [1,2,3,4],
+    'C' : [1,2,3,None],
+    'D' : [None, 2,3,4]
+}
+
+df2 = pd.DataFrame(data2)
+
+# menggunakan interpolate untuk mengisi data kosong
+df2.interpolate(method='linear', inplace=True)  # mengisi data kosong dengan interpolasi   
+# print(df2)
+
+# menggunakan library missingno untuk visualisasi data kosong
+df_pokemon = pd.read_csv('sample_data/Pokemon.csv')
+
+ # menampilkan bar chart dari data kosong
+msno.bar(df_pokemon, figsize=(10, 5), color='blue', fontsize=12)
+
+msno.matrix(df_pokemon)
+plt.show()

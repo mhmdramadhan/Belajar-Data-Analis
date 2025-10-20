df5dc1dde44fe178bb71464094018c793f5afc75
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-10-20 09:29:27 +0700

    Add horizontal span for average rice price in bar plot visualization

--- 9.matplotlib-barplot-scatter.py
+++ 9.matplotlib-barplot-scatter.py
@@ -7,6 +7,13 @@ plt.style.use('tableau-colorblind10')
 bulan = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 harga_beras = [11647, 11990, 12041, 12092, 12102, 12115, 12141, 12265, 13036, 13315, 13380, 13458]
 
+rerata = sum(harga_beras) / len(harga_beras)
+# cara menambahkan garis horizontal dan vertikal
+# plt.axhline(y=rerata, color='r', linestyle='--', label=f'Rerata: {rerata:.2f}')
+# plt.axvline(x=6.5, color='g', linestyle='--', label='Tengah Tahun')
+
+plt.axhspan(0, rerata, facecolor='green', alpha=0.3, label=f'Rerata: {rerata:.2f}')
+
 plt.bar(bulan, harga_beras)
 # buat batasan y axis
 plt.ylim(11000, 14000)
@@ -21,6 +28,7 @@ plt.yticks([11000, 11500, 12000, 12500, 13000, 13500, 14000])
 plt.title("Harga Beras PerKG Tahun 2023")
 plt.xlabel("Bulan")
 plt.ylabel("Rupiah")
+plt.legend()
 plt.show()
 
 
